8c549b96b26e118d605e4230209f069e447e091e
gyovanna <gyovanna.diniz@academico.ifpb.edu.br>
2025-04-29 11:33:35 -0300

    primeiros testes

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,28 @@
 import { Text, View, StyleSheet } from "react-native";
 
+const usuarios = [
+  {id:1,
+    nome: "Gyovanna",
+    cargo: "aluno"
+  },
+  {id:2,
+    nome: "Andre",
+    cargo: "professor"
+  }
+]
+
 export default function Index() {
   return (
     <View
       style={style.container}
     >
-      <Text>OIIEE</Text>
+      {
+        usuarios.map(item => (
+          <View style={style.itemLista}>
+        <Text style={{ fontSize: 20}}> {item.nome}</Text>
+        </View>
+      ))      
+      }
     </View>
   );
 }
